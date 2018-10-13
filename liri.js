64cbe3e59c53f105209f25c04bc9f7d87f29370a
Andrea Halloran <andreahalloran01@gmail.com>
2018-10-12 23:55:14 -0500

    responses to prompt

--- liri.js
+++ liri.js
@@ -7,23 +7,30 @@ const inquirer = require('inquirer')
 var keys = require("./keys");
 
 var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify(keys.spotify); 
- 
+
+var spotify = new Spotify(keys.spotify);
 
 
 // Prompts
 
-inquirer.prompt([
-    {
-        type: "list",
-        message: "What do you want to search for?",
-        name: "choices",
-        choices: ["spotify-this-song", "movie-this", "concert-this"]
+inquirer.prompt([{
+    type: "list",
+    message: "What do you want to search for?",
+    name: "choices",
+    choices: ["spotify-this-song", "movie-this", "concert-this"]
+}]).then(response => {
+    // add if/else statements to pull in the functions 
+    if (response.choices === "spotify-this-song") {
+        console.log("songChoice")
     }
-]).then(response => {
-    
-})
+    if (response.choices === "movie-this") {
+        console.log("okay")
+    }
+    if (response.choices === "concert-this") {
+        console.log("fine")
+    }
+});
 
 
 // Functions 
+
